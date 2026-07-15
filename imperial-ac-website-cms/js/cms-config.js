806722d8d2d3c66f9cc08f99c2e566b116793fe0
flexbot3003 <85273259+flexbot3003@users.noreply.github.com>
2026-07-15 13:39:29 +0200

    Update cms-config.js

--- imperial-ac-website-cms/js/cms-config.js
+++ imperial-ac-website-cms/js/cms-config.js
@@ -1,7 +1,7 @@
 window.IMPERIAL_CMS = {
   // Paste the values from Supabase Project Settings > API.
   // The anon/publishable key is intended for browser use when Row Level Security is enabled.
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://atsyjnveebjwrfptkdtc.supabase.co/rest/v1/",
+  supabaseAnonKey: "sb_publishable__E5ZU54qVR6WumH3Vey3WA_TnNX43mL",
   storageBucket: "club-media"
 };
